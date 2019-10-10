2ec1719cc2867be43f5ba32023b62498d90b68a8
Ajey Pai K <ajeypie@gmail.com>
2019-10-10 11:27:38 -0700

    Update AEP.py

--- AEP.py
+++ AEP.py
@@ -16,60 +16,70 @@ def gen_bernoulli(X, n, p):
     # print(*bernoulli_sequences, sep="\n")
     return bernoulli_sequences
 
-
+def get_prob(sequence):
+  return pow(p,sequence.count(1))*pow((1-p),sequence.count(0))
 
 def entropy(p):
     H = p*(log(1/p)/log(2))+(1-p)*(log(1/(1-p))/log(2))
     return H
 
-
-def get_prob(sequences, n, X, epsilon, H):
-    frequency_count = Counter(sequences)
-    labels, values = zip(*frequency_count.items())
-    values = list(values)
-    indices = np.arange(0, len(labels))
-    print(indices)
-    width = 0.8
-    probabilities = [a/X for a in values]
-    typical_prob = [0] * len(labels)
-    typical_set = []
-    for i in range(0, len(labels)):
-      if (probabilities[i] >= pow(2, -n*(H + epsilon)) and probabilities[i] <= pow(2, -n*(H - epsilon))):
-        typical_set.append(labels[i]) 
-        typical_prob[i] = probabilities[i]
-    print("Total number of typical sequences are:", len(typical_set))
-    #print("The sum of probabilities of all sequences is: %f" % sum(probabilities))
-    #print("\n")
-    # print("The sequences that belong the the typical set are:\n")
-    # print(*typical_set, sep  ="\n")
-    plt.bar(indices, typical_prob, align='center', width=width)
-    plt.xlabel("Sequence number")
-    plt.ylabel("Probability of occurence")
-    # for i, v in enumerate(probabilities):
-    # plt.text(i-0.5, v, str(v), color='blue')
-    plt.show()
-    plt.savefig("baba")
-    return probabilities, H
+def get_list(sequences, n, X, epsilon, H):
+  frequency_count = Counter(sequences)
+  labels, values = zip(*frequency_count.items())
+  indices = np.arange(0, len(labels))
+  width = 0.8
+  probabilities = [a/X for a in values]
+  plt.bar(indices, probabilities, align='center', width=width)
+  plt.xlabel("Sequence number")
+  plt.ylabel("Probability of occurence")
+  plt.legend(['All Sequences'])
+  plt.show()
+  plt.savefig("histogram")
+  # for i, v in enumerate(probabilities):
+  # plt.text(i-0.5, v, str(v), color='blue')
+  plt.show()
+  return labels, indices
+  
+def get_typ_seq(indices, frequent, joint_PMF, epsilon, n, H):
+  typical_prob = [0] * len(frequent)
+  typical_set = []
+  for i in range(0, len(frequent)):
+    if (joint_PMF[i] >= pow(2, -n*(H + epsilon)) and joint_PMF[i] <= pow(2, -n*(H - epsilon))):
+      typical_set.append(frequent[i])
+      typical_prob[i] = joint_PMF[i]
+  plt.bar(indices, typical_prob, align='center', width=0.8)
+  plt.xlabel("Typical sequence number")
+  plt.ylabel("Probability of occurence")
+  plt.legend(['All Sequences','Typical Sequences'])
+  plt.show()
+  plt.savefig("Typical set")
+  print("Total number of typical sequences are:", len(typical_set))
+  print("The sequences that belong the the typical set are:\n")
+  print(*typical_set, sep  ="\n")
+  return
 
 
 if __name__ == '__main__':
-    try:
-        n = int(sys.argv[1])  # length of sequence
-        X = int(sys.argv[2])  # No.of Sequences
-        p = float(sys.argv[3])  # Bernoulli parameter p
-    except Exception as e:
-        n = 8
-        X = 50000
-        p = 0.25
-    finally:
-        print("\n")
-        print("Length of sequence is:", n)
-        print("No.of Sequences is:", X)
-        print("Bernoulli parameter is:", p)
-        print("\n")
+  possible_sequences = []
+  try:
+      n = int(sys.argv[1])  # length of sequence
+      X = int(sys.argv[2])  # No.of Sequences
+      p = float(sys.argv[3])  # Bernoulli parameter p
+  except Exception as e:
+      n = 10
+      X = 50000
+      p = 0.3
+  finally:
+      print("\n")
+      print("Length of sequence is:", n)
+      print("No.of Sequences is:", X)
+      print("Bernoulli parameter is:", p)
+      print("\n")
 H = entropy(p)
 print("The entropy H(X) is: %f bits" % H)
 sequences = gen_bernoulli(X, n, p)
 print("The size of the sample space is: 2 ^", n)
 epsilon = 0.1
-probabilities, H = get_prob(sequences, n, X, epsilon, H)
+frequent, indices = get_list(sequences, n, X, epsilon, H)
+joint_PMF = [get_prob(sequence) for sequence in frequent]
+get_typ_seq(indices, frequent, joint_PMF, epsilon, n, H)
